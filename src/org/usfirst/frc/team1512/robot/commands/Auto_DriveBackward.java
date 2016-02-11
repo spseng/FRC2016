7d297436046328e489ad76d0aaed96064e284b9b
BillBi1999 <ran.bi@sps.edu>
2016-02-11 10:15:58 -0500

    auto backward
    added some parameters

--- src/org/usfirst/frc/team1512/robot/commands/Auto_DriveBackward.java
+++ src/org/usfirst/frc/team1512/robot/commands/Auto_DriveBackward.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team1512.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
+import org.usfirst.frc.team1512.robot.RobotMap;
+import org.usfirst.frc.team1512.robot.subsystems.DriveTrain;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -8,8 +10,9 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class Auto_DriveBackward extends CommandBase {
 
-	boolean finished = false;
-	double speed = 1.0; //this is the default speed.  Second constructor can set speed
+	int timer = 0;    
+	double speed = 1.0; //this is set to full forward special constructor will update speed
+	double timeinseconds=0.1;  //default to tenth of second
 	
     public Auto_DriveBackward() {
         // Use requires() here to declare subsystem dependencies
@@ -24,33 +27,37 @@ public class Auto_DriveBackward extends CommandBase {
     	speed=inspeed;
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
+	public Auto_DriveBackward(double inspeed, double intimeinseconds) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(drive);
+    	speed=inspeed;
+    	timeinseconds=intimeinseconds;
     }
+
+	// Called just before this Command runs the first time
+    protected void initialize() {
+    	setTimeout(timeinseconds);
+       	drive.driveB(speed);
+   }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	drive.driveB(speed);
-    	Timer.delay(0.1);
-    	finished = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-       if (finished = true)
-       {
-    	   return false;
-       }
-       return true;
-    }
+    	return isTimedOut();
+     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	//drive.stop();
+    	drive.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
