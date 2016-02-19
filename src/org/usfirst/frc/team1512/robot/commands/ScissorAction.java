4848c0ad7ef2cb5f1bfd950ad5f7d67b1beb43b5
JR Ereyi <jeanrafael.ereyi@sps.edu>
2016-02-19 08:59:47 -0500

    Feb 19th version of Scissor Lift

---
+++ src/org/usfirst/frc/team1512/robot/commands/ScissorAction.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team1512.robot.commands;
+
+import org.usfirst.frc.team1512.robot.OI;
+import org.usfirst.frc.team1512.robot.subsystems.ArmControl;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ScissorAction extends CommandBase {
+	
+    public ScissorAction() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(cut);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    
+    	double collectvalue=oi.xbox.getRawAxis(3);
+    	
+    	if (collectvalue>0.5)
+    	{
+    		cut.RaiseScissor();
+    	}
+    	
+    	else if (collectvalue<-0.5)
+    	{
+    		cut.LowerScissor();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team1512/robot/subsystems/ScissorSubsystem.java
@@ -0,0 +1,60 @@
+package org.usfirst.frc.team1512.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ScissorSubsystem extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	Compressor main;
+	DoubleSolenoid contract;
+	DoubleSolenoid gripper1;
+	DoubleSolenoid gripper2;
+	
+	ScissorSubsystem cut;
+	
+	public ScissorSubsystem()
+	{
+		super();
+		main = new Compressor();
+//		contract = new DoubleSolenoid(0, 1);
+		scissor1 = new DoubleSolenoid(0, 1);
+	}
+
+	public void Compress()
+	{
+		main.start();
+	}
+	
+	public void endC()
+	{
+		main.stop();
+	}
+
+	public void RaiseScissor())
+	{
+		scissor1.set(Value.kForward);
+	}
+	
+	public void LowerScissor()
+	{
+		scissor1.set(Value.kReverse);
+	}
+
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
+
